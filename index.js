d62c8928b5c3785ca87eeabdfac4962e19199b3b
mnindrazaka <mnindrazaka@gmail.com>
2018-12-26 21:58:33 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const feed = require('medium-json-feed')
+
+app.use(cors())
+app.get('/feed', (req, res) => {
+  feed('wripolinema').then(data => res.json(data))
+})
+
+app.listen(3001, () => console.log('server started'))
